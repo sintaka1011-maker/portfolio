e6d92fb459eb2bfae6a2e2c8e578ef6a4d6a7298
Doonald <sintaka1011@gmail.com>
2026-01-24 17:16:04 +0900

    Fix CTA button click event handling
    Prevent default action for click event after 2 seconds.

--- cta-button-fix/after/script.js
+++ cta-button-fix/after/script.js
@@ -1,11 +1,10 @@
 const ctaBtn = document.querySelector('.cta-btn');
 
 ctaBtn.addEventListener('click', (e) => {
-  if(ctaBtn.classList.contains('is-disabled')) {
-    e.preventDefault();
-    return;
-  }
-
   ctaBtn.classList.add('is-disabled');
   ctaBtn.textContent = '送信中. . .';
+
+  setTimeout(() => {
+   e.preventDefault();
+  }, 2000);
 });
