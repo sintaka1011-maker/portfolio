e7cc2517bed4b37e2f787858f1f6abef64df803a
Doonald <sintaka1011@gmail.com>
2026-01-24 17:08:26 +0900

    Fix typo in querySelector and add click handler

--- cta-button-fix/after/script.js
+++ cta-button-fix/after/script.js
@@ -1,1 +1,11 @@
+const ctaBtn = document.querySlector('.cta-btn');
 
+ctaBtn.addEventListener('click', (e) => {
+  if(ctaBtn.classList.contains('is-disabled')) {
+    e.preventDefault();
+    return;
+  }
+
+  ctaBtn.classList.add('is-disabled');
+  ctaBtn.textContent = '送信中. . .';
+});
