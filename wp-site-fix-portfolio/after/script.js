6c1ad7f75ba567da10f22c790e579fdf669a310e
Doonald <sintaka1011@gmail.com>
2026-02-05 23:31:00 +0900

    Update script.js

--- wp-site-fix-portfolio/after/script.js
+++ wp-site-fix-portfolio/after/script.js
@@ -1,1 +1,9 @@
+const header = document.querySelector('.site-header');
 
+window.addEventListener('scroll', () => {
+  if(window.scrollY > 50) {
+    header.classList.add('is-scrolled');
+  } else {
+    header.classList.remove('is-scrolled');
+  }
+});
