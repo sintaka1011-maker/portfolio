1b17a82307ddafa9b19d18a0bf7c8846e63ef101
Doonald <sintaka1011@gmail.com>
2026-02-23 20:05:31 +0900

    Toggle FAQ answer visibility on question click

--- wp-site-fix-portfolio/after/faq_script.js
+++ wp-site-fix-portfolio/after/faq_script.js
@@ -2,11 +2,8 @@ const faqItems = document.querySelectorAll('.faq-content');
 
 faqItems.forEach(item => {
   const question = item.querySelector('.faq-q');
-  
+  const answer = item.querySelector('.faq-a');
   question.addEventListener('click', () => {
-    faqItems.forEach(i=> {
-      i.querySelector('.faq-a').classList.remove('is-open');
-    });
-    item.querySelector('.faq-a').classList.add('is-open');
+    answer.classList.toggle('is-open');
   });
 });
