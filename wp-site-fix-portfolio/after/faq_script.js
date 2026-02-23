d16197883580d7d3d02249c729f8f6d9b34e7f8c
Doonald <sintaka1011@gmail.com>
2026-02-23 19:18:07 +0900

    Add FAQ toggle functionality in faq_script.js

---
+++ wp-site-fix-portfolio/after/faq_script.js
@@ -0,0 +1,10 @@
+const faqItems = document.querySelectorAll('.faq-content');
+
+faqItems.forEach(item => {
+  const question = item.querySelector('.faq-q');
+  const answer = item.querySelector('.faq-a');
+
+  question.addEventListener('click', () => {
+    answer.classList.toggle('is-open');
+  });
+});
