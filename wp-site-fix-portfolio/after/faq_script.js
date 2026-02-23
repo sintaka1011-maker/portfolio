10f62292f0f0789d64f39ef07c68571eb2abeb1d
Doonald <sintaka1011@gmail.com>
2026-02-23 20:08:55 +0900

    Add click event to toggle FAQ item visibility

--- wp-site-fix-portfolio/after/faq_script.js
+++ wp-site-fix-portfolio/after/faq_script.js
@@ -3,7 +3,9 @@ const faqItems = document.querySelectorAll('.faq-content');
 faqItems.forEach(item => {
   const question = item.querySelector('.faq-q');
   const answer = item.querySelector('.faq-a');
+  
   question.addEventListener('click', () => {
+    item.classList.toggle('open');
     answer.classList.toggle('is-open');
   });
 });
